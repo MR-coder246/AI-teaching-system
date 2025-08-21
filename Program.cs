9026f1662d4ae44eceb108c27975d43b9d06d9c9
MR-coder246 <112022997+MR-coder246@users.noreply.github.com>
2025-08-21 10:40:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Radzen;
+using MyFirstApp.Components;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddRazorComponents().AddInteractiveServerComponents().AddHubOptions(options => options.MaximumReceiveMessageSize = 10 * 1024 * 1024);
+builder.Services.AddControllers();
+builder.Services.AddRadzenComponents();
+builder.Services.AddRadzenCookieThemeService(options =>
+{
+    options.Name = "MyFirstAppTheme";
+    options.Duration = TimeSpan.FromDays(365);
+});
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<MyFirstApp.postgresService>();
+builder.Services.AddDbContext<MyFirstApp.Data.postgresContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("postgresConnection"));
+});
+var app = builder.Build();
+var forwardingOptions = new ForwardedHeadersOptions()
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+};
+forwardingOptions.KnownNetworks.Clear();
+forwardingOptions.KnownProxies.Clear();
+app.UseForwardedHeaders(forwardingOptions);
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+app.Run();
